8f9742efaa6f35fb285ff24b592c916cc76f0593
Oleksandr Prylutskyy <sashaprylutskyy@gmail.com>
2025-01-31 12:24:06 +0200

    Input fields dynamic interaction: if an entered url is valid, password input field is displayed

--- src/main/resources/static/js/submitButton.js
+++ src/main/resources/static/js/submitButton.js
@@ -1,13 +1,20 @@
 const urlField = document.getElementById('url');
 const passwordField = document.getElementById('password');
 const submitButton = document.getElementById('submitButton');
+const passwordContainer = document.querySelector('.password-container');
+const urlInputGroup = document.querySelector('.form-group #url');
+
 const urlPattern = /^(https?:\/\/)?([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,6}(\/[a-zA-Z0-9-]*)*\/?$/;
 
 urlField.addEventListener('input', function() {
     if (urlPattern.test(urlField.value.trim())) {
         submitButton.disabled = false;
+        passwordContainer.classList.remove('hidden');
+        urlInputGroup.style.borderRadius = '15px 15px 0 0';
     } else {
         submitButton.disabled = true;
+        passwordContainer.classList.add('hidden');
+        urlInputGroup.style.borderRadius = '15px';
     }
 });
 
